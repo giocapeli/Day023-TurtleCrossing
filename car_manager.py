12962a8a10601b5bfa09d237c269168174583907
Giovanni Capeli <capeli.andreas@gmail.com>
2023-02-12 15:28:37 +0100

    road

--- car_manager.py
+++ car_manager.py
@@ -12,13 +12,14 @@ class CarManager():
         self.all_cars = []
         
     def create_car(self):
-        car = Turtle("square")
-        car.shapesize(1, 2)
-        car.penup()
-        car.color(choice(COLORS))
-        car.goto(300, randint(-250, 250))
-        car.setheading(180)
-        self.all_cars.append(car)
+        if randint(1, 5) == 1: #pic a random change of appearing a new car
+            car = Turtle("square")
+            car.shapesize(1, 2)
+            car.penup()
+            car.color(choice(COLORS))
+            car.goto(300, randint(-10, 10)*25)
+            car.setheading(180)
+            self.all_cars.append(car)
     
     def move(self):
         for car in self.all_cars:
