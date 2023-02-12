9c661a23703e56f1aab06ac07db1265433733071
Giovanni Capeli <capeli.andreas@gmail.com>
2023-02-12 15:19:32 +0100

    huge traffic

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+from random import choice, randint
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager():
+    
+    def __init__(self):
+        self.all_cars = []
+        
+    def create_car(self):
+        car = Turtle("square")
+        car.shapesize(1, 2)
+        car.penup()
+        car.color(choice(COLORS))
+        car.goto(300, randint(-250, 250))
+        car.setheading(180)
+        self.all_cars.append(car)
+    
+    def move(self):
+        for car in self.all_cars:
+            car.forward(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.colormode(255)
 
 player = Player()
+cars = CarManager()
 
 screen.listen()
 screen.onkeypress(player.move_forward, "w") #dont pass () to the function or it calls itself
@@ -21,6 +23,8 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     
+    cars.create_car()
+    cars.move()
     
 screen.exitonclick()
 
